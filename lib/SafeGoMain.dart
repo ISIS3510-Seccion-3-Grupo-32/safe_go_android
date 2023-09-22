635d870dffaf929204436b25e8f75ed6a29ead06
GwenACL <gwenaelle.ancel@orange.fr>
2023-09-22 17:22:58 -0500

    Needs control of the inputs

--- lib/SafeGoMain.dart
+++ lib/SafeGoMain.dart
@@ -1,115 +1,236 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-            body: Align(
-                child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Container(
-          alignment: Alignment.topCenter,
-          height: 400.0,
-          width: 800.0,
-          color: Colors.transparent,
-          child: Container(
-              decoration: const BoxDecoration(
-                  color: Color(0xFF96CEB4),
-                  borderRadius: BorderRadius.only(
+  runApp(const SafeGo());
+}
+
+class SafeGo extends StatelessWidget {
+  const SafeGo({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Go Safe Testing'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    double fontRegister = screenHeight * 0.05;
+    double fontInputs = screenHeight * 0.04;
+    double paddingSides = screenWidth * 0.05;
+    double paddingBetween = screenHeight * 0.01;
+
+    // Define TextEditingController instances
+    final fullNameController = TextEditingController();
+    final passwordController = TextEditingController();
+    final emailController = TextEditingController();
+    final dobController = TextEditingController();
+
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: FlutterMap(
+                  options: MapOptions(
+                    interactiveFlags: InteractiveFlag.none,
+                    center: LatLng(4.60140465, -74.0649032880709),
+                    zoom: 18.0,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: const ['a', 'b', 'c'],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF96CEB4),
+                    borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50))),
-              child: Column(
-                children: [
-                  RichText(
-                    text: const TextSpan(
-                      text: "  Welcome to SafeGo \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'Login Before you start your trip! \n',
+                      topRight: Radius.circular(50),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: "Register",
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                            fontSize: fontRegister,
                           ),
                         ),
-                        TextSpan(
-                          text: '\nUSER LOCATION DATA GOES HERE!!!',
-                          style: TextStyle(
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(
+                            0, paddingBetween, 0, paddingBetween),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
                             color: Colors.white,
-                            fontSize: 10,
+                            height:
+                                2.0, // Adjust the height of the Divider line as needed
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                  ),
-                  RichText(
-                      textAlign: TextAlign.left,
-                      text: const TextSpan(
-                        text: "  \n \nLogin Credentials \n",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(paddingSides,
+                              paddingBetween, paddingSides, paddingBetween),
+                          child: TextField(
+                            controller:
+                                fullNameController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Full Name",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                            ),
+                          ),
                         ),
-                      )),
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide:
-                                BorderSide(width: 2, color: Colors.grey)),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey),
-                        hintText: "Username",
-                        fillColor: Colors.white70,
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide:
-                                BorderSide(width: 1, color: Colors.white)),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey),
-                        hintText: "Password",
-                        fillColor: Colors.white70,
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(paddingSides,
+                              paddingBetween, paddingSides, paddingBetween),
+                          child: TextField(
+                            controller:
+                                passwordController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Password",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: SizedBox(
-                          width: 251,
-                          height: 40,
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(paddingSides,
+                              paddingBetween, paddingSides, paddingBetween),
+                          child: TextField(
+                            controller:
+                                emailController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Email",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(paddingSides,
+                              paddingBetween, paddingSides, paddingBetween),
+                          child: TextField(
+                            controller: dobController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Date of birth",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(paddingSides,
+                            paddingBetween, paddingSides, paddingBetween),
+                        child: SizedBox(
+                          width: screenWidth * 0.8,
+                          height: screenHeight * 0.06,
                           child: ElevatedButton(
                             onPressed: () {
-                              debugPrint('ElevatedButton Clicked');
+                              // Print the content of the input fields
+                              debugPrint(
+                                  'Full Name: ${fullNameController.text}');
+                              debugPrint(
+                                  'Password: ${passwordController.text}');
+                              debugPrint('Email: ${emailController.text}');
+                              debugPrint(
+                                  'Date of Birth: ${dobController.text}');
                             },
                             child: Text(
-                              'Login',
+                              'Submit',
                               style: TextStyle(color: Colors.black),
                             ),
                             style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.white),
-                          )))
-                ],
-              )),
-        ),
-      ],
-    )))),
-  );
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
