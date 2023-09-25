1e1c4787b34194abc81f3b59da3227b88d25a79c
cesar-mg <c.morenog@uniandes.edu.co>
2023-09-24 21:25:35 -0500

    Improved perfomance/Location sensor V1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'DestinationChoiceView.dart'; // Import your DestinationChoice widget
 import 'RegisterView.dart';
-import 'package:safe_go_dart/logic/PermissionRequest.dart';
 
-import 'SafeGoMap.dart';
 void main() {
   runApp(const SafeGo());
 }
@@ -37,13 +36,9 @@ class SafeGoMain extends StatefulWidget {
 class _SafeGoMainState extends State<SafeGoMain> {
   final fullNameController = TextEditingController();
   final passwordController = TextEditingController();
-  LatLng userLocation = LatLng(0.0,0.0);
-  @override
-  void initState() {
-    super.initState();
-    PermissionRequest request = PermissionRequest(userLocation: userLocation);
-    request.requestLocationPermission();
-  }
+  final _formKey = GlobalKey<FormState>();
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -55,9 +50,22 @@ class _SafeGoMainState extends State<SafeGoMain> {
     return Scaffold(
       body: Column(
         children: [
-          const Flexible(
+          Flexible(
             flex: 2,
-            child: SafeGoMap(),
+            child: FlutterMap(
+              options: MapOptions(
+                interactiveFlags: InteractiveFlag.none,
+                center: LatLng(4.60140465, -74.0649032880709),
+                zoom: 18.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: const ['a', 'b', 'c'],
+                ),
+              ],
+            ),
           ),
           Flexible(
             flex: 3,
@@ -120,126 +128,165 @@ class _SafeGoMainState extends State<SafeGoMain> {
                     color: Colors.white,
                     height: 2.0,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: paddingSides),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Login Credentials',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: fontSubtext,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: paddingSides),
-                        child: TextField(
-                          controller: fullNameController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              borderSide: const BorderSide(
-                                width: 2,
-                                color: Colors.grey,
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: paddingSides),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Login Credentials',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: fontSubtext,
                               ),
                             ),
-                            filled: true,
-                            hintStyle: const TextStyle(color: Colors.grey),
-                            hintText: "Username",
-                            fillColor: Colors.white70,
-                            contentPadding: const EdgeInsets.symmetric(
-                                vertical: 8.0, horizontal: 12.0),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: paddingSides),
-                    child: TextField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          borderSide: const BorderSide(
-                            width: 2,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        filled: true,
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        hintText: "Password",
-                        fillColor: Colors.white70,
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 12.0),
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: SizedBox(
-                      width: screenWidth * 0.8,
-                      height: screenHeight * 0.06,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Print the content of the input fields
-                          debugPrint('Username: ${fullNameController.text}');
-                          debugPrint('Password: ${passwordController.text}');
-
-                          // Redirect to DestinationChoice when the button is clicked
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DestinationChoiceView(),
-                            ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                        ),
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: GestureDetector(
-                      onTap: () {
-                        // Navigate to the registration view
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterView(),
-                          ),
-                        );
-                      },
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text.rich(
-                          TextSpan(
-                            text: "Don't have an account? ",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: fontSubtext,
-                            ),
-                            children: const <TextSpan>[
-                              TextSpan(
-                                text: 'Register',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  decoration: TextDecoration.underline,
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: paddingSides),
+                          child: TextFormField(
+                            controller:
+                                fullNameController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
                                 ),
                               ),
-                            ],
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "UserName",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Username is required';
+                              }
+                              return null;
+                            },
                           ),
                         ),
-                      ),
+                        Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: paddingSides),
+                          child: TextFormField(
+                            controller:
+                                passwordController, // Assign the controller
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: BorderSide(
+                                  width: 2,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              filled: true,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Password",
+                              fillColor: Colors.white70,
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 12.0),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureText
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                  color: Colors.grey,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+                                },
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password is required';
+                              }
+                              return null;
+                            },
+                            obscureText: _obscureText,
+                          ),
+                        ),
+                        Center(
+                          child: SizedBox(
+                            width: screenWidth * 0.8,
+                            height: screenHeight * 0.06,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  // Print the content of the input fields
+                                  debugPrint(
+                                      'Username: ${fullNameController.text}');
+                                  debugPrint(
+                                      'Password: ${passwordController.text}');
+
+                                  // Redirect to DestinationChoice when the button is clicked
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          DestinationChoiceView(),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Text(
+                                'Login',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              // Navigate to the registration view
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => RegisterView(),
+                                ),
+                              );
+                            },
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text.rich(
+                                TextSpan(
+                                  text: "Don't have an account? ",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: fontSubtext,
+                                  ),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'Register',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
